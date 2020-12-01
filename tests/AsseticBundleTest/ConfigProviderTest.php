a24ebdc2eadd445f08903838f2072ced9c6aa445
Fabian Grutschus <f.grutschus@lubyte.de>
2020-12-01 14:30:20 +0100

    Added some tests

---
+++ tests/AsseticBundleTest/ConfigProviderTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AsseticBundleTest;
+
+use PHPUnit\Framework\TestCase;
+use Fabiang\AsseticBundle\ConfigProvider;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Interop\Container\ContainerInterface;
+
+/**
+ * @coversDefaultClass Fabiang\AsseticBundle\ConfigProvider
+ */
+class ConfigProviderTest extends TestCase
+{
+
+    private ConfigProvider $config;
+
+    protected function setUp(): void
+    {
+        $this->config = new ConfigProvider();
+    }
+
+    /**
+     * @covers ::__invoke
+     */
+    public function testInvoke(): void
+    {
+        $this->assertTrue(is_callable($this->config));
+
+        $result = $this->config->__invoke();
+        $this->assertIsArray($result);
+        $this->assertNotEmpty($result);
+        $this->assertArrayNotHasKey('service_manager', $result);
+    }
+
+}
---
+++ tests/AsseticBundleTest/Factory/AsseticMiddlewareFactoryTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AsseticBundleTest;
+
+use PHPUnit\Framework\TestCase;
+use Fabiang\AsseticBundle\AsseticMiddleware;
+use Fabiang\AsseticBundle\Factory\AsseticMiddlewareFactory;
+use Fabiang\AsseticBundle\Service;
+use Laminas\View\Renderer\PhpRenderer;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Interop\Container\ContainerInterface;
+
+/**
+ * @coversDefaultClass Fabiang\AsseticBundle\Factory\AsseticMiddlewareFactory
+ */
+final class AsseticMiddlewareFactoryTest extends TestCase
+{
+
+    use ProphecyTrait;
+
+    private AsseticMiddlewareFactory $factory;
+
+    protected function setUp(): void
+    {
+        $this->factory = new AsseticMiddlewareFactory();
+    }
+
+    /**
+     * @covers ::__invoke
+     */
+    public function testInvoke(): void
+    {
+        $this->assertTrue(is_callable($this->factory));
+
+        $asseticService = $this->prophesize(Service::class);
+        $phpRenderer    = $this->prophesize(PhpRenderer::class);
+
+        $container = $this->prophesize(ContainerInterface::class);
+        $container->get(Service::class)
+            ->shouldBeCalled()
+            ->willReturn($asseticService->reveal());
+
+        $container->has(PhpRenderer::class)
+            ->willReturn(true);
+
+        $container->get(PhpRenderer::class)
+            ->shouldBeCalled()
+            ->willReturn($phpRenderer->reveal());
+
+        $this->assertInstanceOf(
+            AsseticMiddleware::class,
+            $this->factory->__invoke($container->reveal(), AsseticMiddleware::class, [])
+        );
+    }
+
+}
